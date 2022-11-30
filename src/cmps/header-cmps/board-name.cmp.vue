f77b893566579e2e799c429cf7eb77c7b41c0726
Boaz Deri <boazderi@gmail.com>
2022-11-30 12:50:44 +0200

    boaz -create new header cmps abd load element plus

--- src/cmps/header-cmps/board-name.cmp.vue
+++ src/cmps/header-cmps/board-name.cmp.vue
@@ -1,4 +1,12 @@
 <template>
+<div>
+    <h1
+    class="el-tooltip__trigger"
+    contenteditable=true
+    >
+        project-name
+    </h1>
+</div>
   <el-tooltip content="I am an el-tooltip">
     <el-button>Name </el-button>
   </el-tooltip>
