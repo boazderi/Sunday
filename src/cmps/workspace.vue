79f79b0d6bab4550e572e27c91a9cf83836e37a3
Arnon Arditi <arnonrdt1234@gmail.com>
2022-11-30 08:45:44 +0200

    a

--- src/cmps/workspace.vue
+++ src/cmps/workspace.vue
@@ -1,13 +1,27 @@
 <template>
   <section class="workspace">
-    <h1>workspace</h1>
+    <section class="details">
+      <h1>workspace</h1>
+
+    </section>
+    <button @click="(isOpen = !isOpen)">toggle</button>
 
   </section>
 </template>
 
 <script>
-export default {
 
+export default {
+  data() {
+    return {
+      boards: [],
+      isOpen: true,
+    }
+  },
+  created() {
+    this.boards = this.$store.getters.boards
+    console.log(this.boards)
+  }
 
 }
 </script>
