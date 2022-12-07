44366eab106b44423b7376c4dc11d0a79f2383f6
Boaz Deri <boazderi@gmail.com>
2022-12-07 15:54:51 +0200

    b charts improves

--- src/cmps/header-cmps/board-name.cmp.vue
+++ src/cmps/header-cmps/board-name.cmp.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="flex items-center">
-      <el-tooltip content="Click to edit">
+      <el-tooltip transition="none" auto-close=0 content="Click to edit">
         <h1 class="board-name-title el-tooltip__trigger" contenteditable>
           Sprint 4
         </h1>
       </el-tooltip>
-      <el-tooltip content="Show board description">
+      <el-tooltip transition="none" auto-close=0 content="Show board description">
         <button class="icon outboard-hover board-name-icon" v-icon="'info'"></button>
       </el-tooltip>
-      <el-tooltip content="Add to favorites ">
+      <el-tooltip transition="none" auto-close=0 content="Add to favorites ">
         <button class="icon outboard-hover board-name-icon" v-icon="'star'"></button>
       </el-tooltip>
   </section>
