2e3623ccd8032460fd5d1a8738b9d6127ec6bad6
Boaz Deri <boazderi@gmail.com>
2022-12-09 00:04:19 +0200

    add filters

--- src/cmps/dashboard-cmps/cards-chart.cmp.vue
+++ src/cmps/dashboard-cmps/cards-chart.cmp.vue
@@ -2,8 +2,11 @@
   <section
     v-if="isDataSection"
     @click="toggleSections"
-    class="dashboard-stats chart-container flex"
+    title="Click to switch"
+    class="dashboard-stats chart-container flex flex-col"
   >
+  <h3 class="cards-data-title"> Data Cards </h3>
+  <div class="flex cards-stats-container">
     <article class="stat flex">
       <h4 class="card-title-item">Total board tasks</h4>
       <h2>{{ this.data.tasks }}</h2>
@@ -16,12 +19,17 @@
       <h4 class="card-title-item">Total board groups</h4>
       <h2>{{ this.data.groups }}</h2>
     </article>
+      </div>
   </section>
   <section
     v-else
     @click="toggleSections"
-    class="dashboard-stats chart-container flex"
+    title="Click to switch"
+    class="dashboard-stats chart-container flex flex-col"
   >
+    <h3 class="cards-data-title"> Members Cards </h3>
+  <div class="flex cards-stats-container">
+
     <article class="stat flex">
       <h4 class="card-title-item">Tal Liber</h4>
       <div><img class="memberDashPic" :src="this.members[0].imgUrl" alt="" /></div>
@@ -34,6 +42,7 @@
         <h4 class="card-title-item">Arnon Ardity</h4>
         <div><img class="memberDashPic" :src="this.members[1].imgUrl" alt="" /></div>
     </article>
+          </div>
   </section>
 </template>
 
